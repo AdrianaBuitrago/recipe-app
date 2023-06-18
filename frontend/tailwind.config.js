dce539cab6cc8c4a9d75aa76bcd035e51ac9db14
Alejandro de Frutos <adefrutoscasado@gmail.com>
2023-06-18 16:31:12 +0200

    Configure tailwind css

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: false, // or 'media' or 'class'
+  content: [
+    './src/**/*.{html,js,ts,jsx,tsx}',
+  ],
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: []
+}
