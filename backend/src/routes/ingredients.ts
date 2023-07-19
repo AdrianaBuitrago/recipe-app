65ded17204a59ff96b5185de0672914d1299ca47
AdrianaBuitrago <buitragoadr@gmail.com>
2023-07-19 15:50:39 +0200

    Add is_checked field to ingredient

--- backend/src/routes/ingredients.ts
+++ backend/src/routes/ingredients.ts
@@ -1,6 +1,7 @@
 import express from 'express'
 import { errorHandler } from '../utils'
 import { Ingredient } from '../models/db'
+import NotFoundError from '../errors/BadRequestError'
 
 
 const router = express()
@@ -10,6 +11,42 @@ router.get('/ingredients', errorHandler(async (req, res) => {
     .query()
 
   res.json(ingredients)
+}))
+
+router.patch('/ingredients/:id(\\d+)', errorHandler(async (req, res) => {
+  const { id } = req.params
+  const { body } = req
+
+  const oldIngredient = await Ingredient
+    .query()
+    .findById(parseInt(id))
+
+  if (!oldIngredient) {
+    throw new NotFoundError('Ingredient not found')
+  }
+
+  const ingredient = await oldIngredient
+    .$query()
+    .patch(body)
+    .findById(parseInt(id))
+    .returning('*')
+
+  res.json(ingredient)
+}))
+
+router.delete('/ingredients/:id(\\d+)', errorHandler(async (req, res) => {
+  const { id } = req.params
+
+  const deleted = await Ingredient
+    .query()
+    .deleteById(id)
+
+  // If not found, the value of deleted is zero
+  if (!deleted) {
+    throw new NotFoundError('Ingredient not found')
+  }
+
+  res.json({message: 'Deleted succesfully'})
 }))
 
 export default router
